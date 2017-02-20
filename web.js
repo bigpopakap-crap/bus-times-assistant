c77c87ee32ebf515e947c4e4e421714a0ecd2145
Kapil Easwar <bigpopakap@gmail.com>
2017-02-20 14:09:20 -0800

    Make it work

--- web.js
+++ web.js
@@ -3,9 +3,9 @@
 // Enable actions client library debugging
 process.env.DEBUG = 'actions-on-google:*';
 
-let Assistant = require('actions-on-google');
-let express = require('express');
-let bodyParser = require('body-parser');
+const ApiAiAssistant = require('actions-on-google').ApiAiAssistant;
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.set('port', (process.env.PORT || 8080));
@@ -13,18 +13,22 @@ app.use(bodyParser.json({type: 'application/json'}));
 
 const GET_12_CESAR_CHAVEZ_TIMES = 'get_12_cesar_chaves_times';
 
-const actionMap = new Map();
-actionMap.set(GET_12_CESAR_CHAVEZ_TIMES, function(assistant) {
-  assistant.tell('Hey, I am in development. What do you expect of me?');
-});
-
 app.get('/', function(request, response) {
   response.sendStatus(200);
 });
 
 app.post('/', function (request, response) {
-  const assistant = new Assistant({request: request, response: response});
+  console.log('KAPIL 1');
+  const assistant = new ApiAiAssistant({request: request, response: response});
+  console.log('KAPIL 2');
+
+  const actionMap = new Map();
+  actionMap.set(GET_12_CESAR_CHAVEZ_TIMES, function(assistant) {
+    assistant.tell('Hey, I am in development. What do you expect of me?');
+  });
+
   assistant.handleRequest(actionMap);
+
   response.sendStatus(200);
 });
 
