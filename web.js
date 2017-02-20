940b235ce5e8e80b3b0d21b7d1f471310e471d45
Kapil Easwar <bigpopakap@gmail.com>
2017-02-20 15:14:50 -0800

    Actually get responses for the 12 from Folsom and Cesar Chavez towards downtown

--- web.js
+++ web.js
@@ -3,33 +3,63 @@
 // Enable actions client library debugging
 process.env.DEBUG = 'actions-on-google:*';
 
-const ApiAiAssistant = require('actions-on-google').ApiAiAssistant;
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request-json');
 
 const app = express();
 app.set('port', (process.env.PORT || 8080));
 app.use(bodyParser.json({type: 'application/json'}));
 
+const nbClient = request.createClient('http://restbus.info/');
+
+const ApiAiAssistant = require('actions-on-google').ApiAiAssistant;
 const GET_12_CESAR_CHAVEZ_TIMES = 'get_12_cesar_chavez_times';
+
+function generatePredictionResponse(p) {
+  const pTypeLabel = p.isScheduleBased ? 'is scheduled to arrive' : 'will arrive';
+  const minuteLabel = p.minutes == 1 ? 'minute' : 'minutes';
+  return `${pTypeLabel} in ${p.minutes} ${minuteLabel}`;
+}
+
+function handle12CesarChavezTimes(assistant) {
+  nbClient.get('/api/agencies/sf-muni/routes/12/stops/7552/predictions', function(err, res, body) {
+    if (err) {
+      assistant.tell('There was an error connecting to NextBus.');
+    } else {
+      const allPredictions = body[0].values;
+      const inboundPredictions = allPredictions.filter(p => p.direction.id === '12___I_F00');
+      const sortedInboundPredictions = inboundPredictions.sort((a, b) => a.epochTime - b.epochTime);
+
+      if (sortedInboundPredictions.length <= 0) {
+        assistant.tell('No predictions found.');
+      } else {
+        const p1 = sortedInboundPredictions[0];
+        const p1Response = generatePredictionResponse(p1);
+
+        let response = `Next 12 from Cesar Chavez and Folsom to downtown ${p1Response}.`;
+
+        if (sortedInboundPredictions.length > 1) {
+          const p2 = sortedInboundPredictions[1];
+          const p2Response = generatePredictionResponse(p2);
+          response = `${response} After that, the next bus ${p2Response}.`
+        }
+
+        assistant.tell(response);
+      }
+    }
+  });
+}
 
 app.get('/', function(request, response) {
   response.sendStatus(200);
 });
 
 app.post('/', function (request, response) {
-  console.log(`
-    Handling request\n
-    headers: ${JSON.stringify(request.headers)}\n
-    body: ${JSON.stringify(request.body)}
-  `);
-
   const assistant = new ApiAiAssistant({request: request, response: response});
 
   const actionMap = new Map();
-  actionMap.set(GET_12_CESAR_CHAVEZ_TIMES, function(assistant) {
-    assistant.tell('Hey, I am in development. What do you expect of me?');
-  });
+  actionMap.set(GET_12_CESAR_CHAVEZ_TIMES, handle12CesarChavezTimes);
 
   assistant.handleRequest(actionMap);
 });
