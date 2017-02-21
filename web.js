e32eb8656f64d9e57e6f1ac242c0b8e36b44857f
Kapil Easwar <bigpopakap@gmail.com>
2017-02-20 19:24:54 -0800

    Created stub for generic API endpoint

--- web.js
+++ web.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json({type: 'application/json'}));
 const nbClient = request.createClient('http://restbus.info/');
 
 const ApiAiAssistant = require('actions-on-google').ApiAiAssistant;
-const GET_12_CESAR_CHAVEZ_TIMES = 'get_12_cesar_chavez_times';
+const GET_NEAREST_BUS_TIMES_BY_ROUTE = 'get_nearest_bus_times_by_route';
 
 function generatePredictionResponse(p) {
   const pTypeLabel = p.isScheduleBased ? 'is scheduled to arrive' : 'will arrive';
@@ -22,33 +22,8 @@ function generatePredictionResponse(p) {
   return `${pTypeLabel} in ${p.minutes} ${minuteLabel}`;
 }
 
-function handle12CesarChavezTimes(assistant) {
-  nbClient.get('/api/agencies/sf-muni/routes/12/stops/7552/predictions', function(err, res, body) {
-    if (err) {
-      assistant.tell('There was an error connecting to NextBus.');
-    } else {
-      const allPredictions = body[0].values;
-      const inboundPredictions = allPredictions.filter(p => p.direction.id === '12___I_F00');
-      const sortedInboundPredictions = inboundPredictions.sort((a, b) => a.epochTime - b.epochTime);
-
-      if (sortedInboundPredictions.length <= 0) {
-        assistant.tell('No predictions found.');
-      } else {
-        const p1 = sortedInboundPredictions[0];
-        const p1Response = generatePredictionResponse(p1);
-
-        let response = `Next 12 from Cesar Chavez and Folsom to downtown ${p1Response}.`;
-
-        if (sortedInboundPredictions.length > 1) {
-          const p2 = sortedInboundPredictions[1];
-          const p2Response = generatePredictionResponse(p2);
-          response = `${response} After that, the next bus ${p2Response}.`
-        }
-
-        assistant.tell(response);
-      }
-    }
-  });
+function handleNearestBusTimesByRoute(assistant) {
+  assistant.tell('It worked!');
 }
 
 app.get('/', function(request, response) {
@@ -59,7 +34,7 @@ app.post('/', function (request, response) {
   const assistant = new ApiAiAssistant({request: request, response: response});
 
   const actionMap = new Map();
-  actionMap.set(GET_12_CESAR_CHAVEZ_TIMES, handle12CesarChavezTimes);
+  actionMap.set(GET_NEAREST_BUS_TIMES_BY_ROUTE, handleNearestBusTimesByRoute);
 
   assistant.handleRequest(actionMap);
 });
