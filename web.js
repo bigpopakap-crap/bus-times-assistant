ad9a06047129405d64ea1c556bfd183c343fbc24
Kapil Easwar <bigpopakap@gmail.com>
2017-02-22 13:05:10 -0800

    Use config vars specified by Heroku

--- web.js
+++ web.js
@@ -1,13 +1,15 @@
 'use strict';
 
-process.env.DEBUG = 'actions-on-google:*';
+// default the config vars
+process.env.DEBUG = process.env.DEBUG || 'actions-on-google:*';
+process.env.PORT = process.env.PORT || 8080;
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request-json');
 
 const app = express();
-app.set('port', (process.env.PORT || 8080));
+app.set('port', process.env.PORT);
 app.use(bodyParser.json({ type: 'application/json' }));
 
 const nbClient = request.createClient('http://restbus.info/');
