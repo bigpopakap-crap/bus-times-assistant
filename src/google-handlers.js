97c8a45037c77d2e6ee14d06f7e922a10653182b
Kapil Easwar <bigpopakap@gmail.com>
2017-03-08 19:22:29 -0800

    Separate out query and response generation from platform-specific code

--- src/google-handlers.js
+++ src/google-handlers.js
@@ -1,23 +1,7 @@
 'use strict';
 
 const { busDirectionFromInput } = require('./ai-config-busDirection.js');
-const { pluralPhrase } = require('./utils.js');
-const { NEXTBUS_ERRORS, getNearestStopResult } = require('./nextbus-adapter.js');
-
-function genericError(assistant) {
-  assistant.tell('Sorry, there was an error. Please try again.');
-}
-
-function generatePredictionResponse(p) {
-  // special case for arriving
-  if (p.minutes === 0) {
-    return p.isScheduleBased ? 'is scheduled to arrive now' : 'is arriving now';
-  } else {
-    const pTypeLabel = p.isScheduleBased ? 'is scheduled to arrive' : 'will arrive';
-    const minutePhrase = pluralPhrase(p.minutes, 'minute', 'minutes');
-    return `${pTypeLabel} in ${minutePhrase}`;
-  }
-}
+const { reportNearestStopResult } = require('./nextbus-assistant.js');
 
 function handleAskForPermission(assistant) {
   // TODO(kapil) validate that direction is a valid enum, and route is valid
@@ -39,50 +23,12 @@ function handleNearestBusTimesByRoute(assistant) {
     return;
   }
 
-  // TODO(kapil) is there a cleaner way to get these params?
+  const deviceLocation = assistant.getDeviceLocation();
   const busRoute = assistant.data.busRoute;
   const busDirection = assistant.data.busDirection;
 
-  const deviceLocation = assistant.getDeviceLocation();
-
-  getNearestStopResult(deviceLocation, busRoute, busDirection, function(err, result) {
-    if (err) {
-      switch (err) {
-        case NEXTBUS_ERRORS.NOT_FOUND:
-          assistant.tell(`No nearby stops found for ${busDirection} route ${busRoute}.`);
-          break;
-        default:
-          genericError(assistant);
-          break;
-      }
-
-      return;
-    }
-
-    const predictions = (result && result.values) || [];
-    if (predictions.length <= 0) {
-      assistant.tell(`No predictions found for ${busDirection} route ${busRoute}`);
-      return;
-    }
-
-    const p1 = predictions[0];
-    const p1Response = generatePredictionResponse(p1);
-
-    let response = `The next ${busDirection} ${busRoute} from ${result.busStop} ${p1Response}`;
-
-    if (predictions.length > 1) {
-      const p2 = predictions[1];
-
-      if (p2.isScheduleBased) {
-        const p2Response = generatePredictionResponse(p2);
-        response = `${response}. After that, the next one ${p2Response}`;
-      } else {
-        const minutePhrase = pluralPhrase(p2.minutes, 'minute', 'minutes');
-        response = `${response}, then again in ${minutePhrase}`;
-      }
-    }
-
-    assistant.tell(`${response}.`);
+  reportNearestStopResult(deviceLocation, busRoute, busDirection, function(response) {
+    assistant.tell(response);
   });
 }
 
---
+++ src/nextbus-assistant.js
@@ -0,0 +1,61 @@
+const { NEXTBUS_ERRORS, getNearestStopResult } = require('./nextbus-adapter.js');
+const { pluralPhrase } = require('./utils.js');
+
+const GENERIC_ERROR_RESPONSE = 'Sorry, there was an error. Please try again.';
+
+function generatePredictionResponse(p) {
+  // special case for arriving
+  if (p.minutes === 0) {
+    return p.isScheduleBased ? 'is scheduled to arrive now' : 'is arriving now';
+  } else {
+    const pTypeLabel = p.isScheduleBased ? 'is scheduled to arrive' : 'will arrive';
+    const minutePhrase = pluralPhrase(p.minutes, 'minute', 'minutes');
+    return `${pTypeLabel} in ${minutePhrase}`;
+  }
+}
+
+function reportNearestStopResult(deviceLocation, busRoute, busDirection, respondCallback) {
+  getNearestStopResult(deviceLocation, busRoute, busDirection, function(err, result) {
+    if (err) {
+      switch (err) {
+        case NEXTBUS_ERRORS.NOT_FOUND:
+          respondCallback(`No nearby stops found for ${busDirection} route ${busRoute}.`);
+          break;
+        default:
+          respondCallback(GENERIC_ERROR_RESPONSE);
+          break;
+      }
+
+      return;
+    }
+
+    const predictions = (result && result.values) || [];
+    if (predictions.length <= 0) {
+      respondCallback(`No predictions found for ${busDirection} route ${busRoute}`);
+      return;
+    }
+
+    const p1 = predictions[0];
+    const p1Response = generatePredictionResponse(p1);
+
+    let response = `The next ${busDirection} ${busRoute} from ${result.busStop} ${p1Response}`;
+
+    if (predictions.length > 1) {
+      const p2 = predictions[1];
+
+      if (p2.isScheduleBased) {
+        const p2Response = generatePredictionResponse(p2);
+        response = `${response}. After that, the next one ${p2Response}`;
+      } else {
+        const minutePhrase = pluralPhrase(p2.minutes, 'minute', 'minutes');
+        response = `${response}, then again in ${minutePhrase}`;
+      }
+    }
+
+    respondCallback(`${response}.`);
+  });
+}
+
+module.exports = {
+  reportNearestStopResult
+};
