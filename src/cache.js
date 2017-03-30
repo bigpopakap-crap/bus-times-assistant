999ac39c8f972438149782652c7a4eab834bb516
Kapil Easwar <bigpopakap@gmail.com>
2017-03-30 16:07:24 -0700

    Add ability to rotate responses in string response module

---
+++ src/cache.js
@@ -0,0 +1,55 @@
+/* global module */
+// TODO add logging to the cache, including latency for pruning and stuff
+
+function CacheItem(key, data, expiryDate) {
+  this.key = key;
+  this.data = data;
+  this.expiryDate = expiryDate;
+}
+
+CacheItem.getKey = function() { return this.key; };
+CacheItem.getData = function() { return this.data; };
+CacheItem.getExpiryDate = function() { return this.expiryDate; };
+
+function Cache() {
+  this.data = {};
+}
+
+Cache.init = function() {
+  return new Cache();
+}
+
+Cache.prune = function() {
+  const now = new Date().getTime();
+
+  Object.keys(this.data).forEach(key => {
+    const cacheItem = this.data[key];
+
+    if (now > cacheItem.getExpiryDate().getTime()) {
+      delete this.data[key];
+    }
+  });
+};
+
+Cache.prototype.set = function(key, data = {}, minutesOfLife = 30) {
+  this.prune();
+  const expiryDate = new Date(new Date().getTime() + minutesOfLife * 60000);
+  this.data[key] = new CacheItem(key, data, expiryDate);
+};
+
+Cache.prototype.clear = function(key) {
+  this.prune();
+  delete this.data[key];
+};
+
+Cache.prototype.get = function(key) {
+  this.prune();
+  return this.data[key].getData();
+};
+
+Cache.prototype.has = function(key) {
+  this.prune();
+  return Boolean(this.data[key]);
+};
+
+module.exports = Cache;
--- src/respond.js
+++ src/respond.js
@@ -2,6 +2,9 @@
 const responses = require('./responses.js');
 const logger = require('./logger.js').forComponent('respond');
 
+// This cache is global across all respond objects, so we initialize it here
+const cache = require('./cache.js').init();
+
 // Alexa doesn't like ampersands in SSML
 function cleanResponse(response) {
   return response.replace(/&/g, 'and');
@@ -36,10 +39,51 @@ function Respond(requestContext) {
   this.logger = logger.forRequest(requestContext);
 }
 
+/* THIS SHOULD BE PRIVATE */
+Respond.prototype.rotateResponse = function(responseKey, responses) {
+  this.logger.debug('pre_rotate_response', {
+    responseKey
+  });
+
+  if (!responses || !responses.length) {
+    this.logger.debug('post_rotate_response', {
+      responseKey,
+      success: false,
+      error: 'array is empty or undefined'
+    });
+    return null;
+  }
+
+  const userId = this.requestContext.getUserId();
+  const cacheKey = `${userId}.${responseKey}.nextIndex`;
+
+  if (!cache.has(cacheKey)) {
+    cache.set(cacheKey, 0);
+  }
+
+  const index = cache.get(cacheKey);
+  const modIndex = index % responses.length;
+
+  cache.set(cacheKey, modIndex + 1);
+
+  this.logger.debug('post_rotate_response', {
+    responseKey,
+    success: true,
+    index,
+    modIndex
+  });
+
+  return responses[index % responses.length];
+};
+
 Respond.prototype.saying = function(responseKey, params = {}) {
   let response = responses[responseKey];
   if (typeof response === 'function') {
     response = response(params);
+  }
+
+  if (Array.isArray(response)) {
+    response = this.rotateResponse(responseKey, response);
   }
 
   if (!response) {
@@ -47,7 +91,7 @@ Respond.prototype.saying = function(responseKey, params = {}) {
       responseKey,
       responseParams: JSON.stringify(params)
     });
-    return responses['keyMissing'];
+    return responses.keyMissing;
   }
 
   const { result, missingParams } = replaceParams(response, params);
