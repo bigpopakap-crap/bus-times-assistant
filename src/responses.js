3806af34c2c20302d001010030f93b90fa391217
Kapil Easwar <bigpopakap@gmail.com>
2017-04-08 10:38:06 -0700

    reformat responses file

--- src/responses.js
+++ src/responses.js
@@ -56,39 +56,99 @@ function getBusTimesString({
 }
 
 module.exports = {
+  /* WELCOME **************************************************************/
   'welcome': [
     a('Hello there! I can look up bus times for you. For example, you can say, "when is the next 12 to downtown?"'),
     a('Hello! You can ask me for bus times. For example, you can say, "when is the next outbound N?"')
   ],
-  'welcome.noLocation': a('Hello there! I can look up bus times for you. For example, you can say, "when is the next 12 to downtown?". But first, you\'ll need to tell me your location by saying "set my location."'),
 
-  // TODO make the help responses
-  'help':             a('Hey! Hope I have been useful to you.'),
-  'help.noLocation':  a('Hey! Hope I have been useful to you. You can start by letting me know where you are so I can look up bus times near you. Try saying "set my location."'),
+  'welcome.noLocation': [
+    a('Hello there! I can look up bus times for you. For example, you can say, "when is the next 12 to downtown?". But first, you\'ll need to tell me your location by saying "set my location."')
+  ],
 
-  'getLocation':                           a('Your location is set to <say-as interpret-as="address">{{address}}</say-as>.'),
-  // TODO use an example address as an example command
-  'getLocation.noLocation':                a('You haven\'t set a location yet. You can ask me to set a location by saying "set my location".'),
-  'getLocation.noLocation.deviceLocation': a('You haven\'t set a location yet. Simply ask for bus times to use your device location, or say "set my location".'),
+  /* HELP *****************************************************************/
+  'help': [
+    a('Hey! Hope I have been useful to you.')
+  ],
 
-  'updateLocation':                 a('There. Your location is now set to <say-as interpret-as="address">{{address}}</say-as>.'),
-  'updateLocation.missingAddress':  a(`You must specify an address. For example, you can say "update my location to ${EXAMPLE_ADDRESS}."`),
-  'updateLocation.locationWarning': a(`There. Your location is now set to <say-as interpret-as="address">{{address}}</say-as>. ${LOCATION_WARNING}`),
-  'updateLocation.notFound':        a('Hmm. I could not find that address. Try saying the full address again, including the city.'),
+  'help.noLocation': [
+    a('Hey! Hope I have been useful to you. You can start by letting me know where you are so I can look up bus times near you. Try saying "set my location."')
+  ],
 
-  'getBusTimes':                                getBusTimesString,
-  'getBusTimes.missingBusDirection':            a('You must specify a direction. For example, "when is the next 12 to downtown?" or "when is the next inbound 12?"'),
-  'getBusTimes.missingBusRoute':                a('You must specify a bus route. For example, "when is the next inbound J?" or "when is the next 14 to downtown?"'),
-  'getBusTimes.missingLocation':                a('You haven\'t set your location yet. To do so, simply say "set my location."'),
-  'getBusTimes.noPredictions':                  a('No predictions found for <w role="ivona:NN">{{busDirection}}</w> route <w role="ivona:NN">{{busRoute}}</w> within 1.5 miles of your location.'),
-  'getBusTimes.noPredictions.locationWarning':  a(`No predictions found for <w role="ivona:NN">{{busDirection}}</w> route <w role="ivona:NN">{{busRoute}}</w> within 1.5 miles of your location. ${LOCATION_WARNING}`),
+  /* GET MY LOCATION ******************************************************/
+  'getLocation': [
+    a('Your location is set to <say-as interpret-as="address">{{address}}</say-as>.')
+  ],
 
-  'locationPermission.request.google': a('To look up routes near you', false),
-  'locationPermission.denialWarning':  a('To proceed, I\'ll need your location. If you do not want to grant permission, you can set your address manually by saying "set my location."'),
+  'getLocation.noLocation': [
+    a('You haven\'t set a location yet. You can ask me to set a location by saying "set my location".')
+  ],
 
-  'error.generic':                  a('Sorry, there was an unexpected error. Please try again.'),
+  'getLocation.noLocation.deviceLocation': [
+    a('You haven\'t set a location yet. Simply ask for bus times to use your device location, or say "set my location".')
+  ],
+
+  /* UPDATE MY LOCATION *****************************************************/
+  'updateLocation': [
+    a('There. Your location is now set to <say-as interpret-as="address">{{address}}</say-as>.')
+  ],
+
+  'updateLocation.missingAddress': [
+    a(`You must specify an address. For example, you can say "update my location to ${EXAMPLE_ADDRESS}."`)
+  ],
+
+  'updateLocation.locationWarning': [
+    a(`There. Your location is now set to <say-as interpret-as="address">{{address}}</say-as>. ${LOCATION_WARNING}`)
+  ],
+
+  'updateLocation.notFound': [
+    a('Hmm. I could not find that address. Try saying the full address again, including the city.')
+  ],
+
+  /* GET BUS TIMES *********************************************************/
+  'getBusTimes': getBusTimesString,
+
+  'getBusTimes.missingBusDirection': [
+    a('You must specify a direction. For example, "when is the next 12 to downtown?" or "when is the next inbound 12?"')
+  ],
+
+  'getBusTimes.missingBusRoute': [
+    a('You must specify a bus route. For example, "when is the next inbound J?" or "when is the next 14 to downtown?"')
+  ],
+
+  'getBusTimes.missingLocation': [
+    a('You haven\'t set your location yet. To do so, simply say "set my location."')
+  ],
+
+  'getBusTimes.noPredictions': [
+    a('No predictions found for <w role="ivona:NN">{{busDirection}}</w> route <w role="ivona:NN">{{busRoute}}</w> within 1.5 miles of your location.')
+  ],
+
+  'getBusTimes.noPredictions.locationWarning': [
+    a(`No predictions found for <w role="ivona:NN">{{busDirection}}</w> route <w role="ivona:NN">{{busRoute}}</w> within 1.5 miles of your location. ${LOCATION_WARNING}`)
+  ],
+
+  /* LOCATION PERMISSION REQUEST ************************************************/
+  'locationPermission.request.google': [
+    a('To look up routes near you', false)
+  ],
+
+  'locationPermission.denialWarning': [
+    a('To proceed, I\'ll need your location. If you do not want to grant permission, you can set your address manually by saying "set my location."')
+  ],
+
+  /* GENERIC ERRORS **************************************************************/
+  'error.generic': [
+    a('Sorry, there was an unexpected error. Please try again.')
+  ],
+
   // TODO should we put the location warning in the generic error case?
-  'error.generic.locationWarning':  a('Sorry, there was an unexpected error. Please try again.'),
+  'error.generic.locationWarning': [
+    a('Sorry, there was an unexpected error. Please try again.')
+  ],
 
-  'keyMissing':     a('I am at a loss for words. Something went wrong. Please try again in a few seconds.')
+  /* FALLBACK FOR MISSING STRINGS ************************************************/
+  'keyMissing': [
+    a('I am at a loss for words. Something went wrong. Please try again in a few seconds.')
+  ]
 };
